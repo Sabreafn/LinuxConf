927e8121cdeba7a0fc38203f6faa3b8d72295595
Sabreafn <132608523+Sabreafn@users.noreply.github.com>
2023-06-04 19:36:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 dir=$(pwd)
 
+# Small Conf
+echo "deb https://mega.nz/linux/MEGAsync/Debian_11.0/ ./" | sudo tee /etc/apt/sources.list.d/megasync.list
+wget -qO - https://mega.nz/linux/MEGAsync/Debian_11.0/Release.key | sudo apt-key add -
+
+sudo apt update
+clear
+
 # List of packages to install                                              
 packages=(                                                                 
     nala
     falkon
     bleachbit
     uget
+    megasync
     git
     net-tools
     default-jre
@@ -14,11 +22,13 @@ packages=(
     exploitdb
     )
 
+# Auto
 for package in "${packages[@]}"                                            
 do                                                                         
     sudo apt-get -y install "$package"                                     
 done
 
+# Manual Commands
 searchsploit -u
 clear
                                                                            
